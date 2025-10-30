38df8d420d94456684f023741683342b49ef9779
devangelwings <khs96523@gmail.com>
2025-10-30 10:31:12 +0900

    COMMENT: 주석 내용 수정
    주석 작성자 추가 및 도메인 용어 정의에 맞게 단어 수정
    
    See also: #1ª

--- src/main/java/com/sparta/deliveryi/global/infrastructure/audit/SpringSecurityAuditorAware.java
+++ src/main/java/com/sparta/deliveryi/global/infrastructure/audit/SpringSecurityAuditorAware.java
@@ -12,19 +12,20 @@ import java.util.Optional;
  * Spring Data JPA의 감사(audit) 기능을 위한 AuditorAware 구현체.
  *
  * <p>
- * 현재 인증된 사용자의 아이디(username)를 생성 및 수정자(auditor) 정보로 제공합니다.
+ * 현재 인증된 회원의 계정(username)을 생성 및 수정자(auditor) 정보로 제공합니다.
  * 인증 정보가 없거나 인증되지 않은 경우 빈 Optional을 반환합니다.
  * </p>
  *
+ * @author 김형섭
  * @since 2025.10.30
  */
 @Component
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
     /**
      * 현재 보안 컨텍스트(SecurityContext)에 저장된 인증(Authentication) 객체로부터
-     * 현재 사용자의 이름을 가져와 감사자(auditor)로 반환합니다.
+     * 현재 회원의 계정을 가져와 감사자(auditor)로 반환합니다.
      *
-     * @return 인증된 사용자의 이름을 담은 Optional, 인증 정보가 없거나 인증되지 않은 경우 빈 Optional
+     * @return 인증된 회원의 계정을 담은 Optional, 인증 정보가 없거나 인증되지 않은 경우 빈 Optional
      */
     @NonNull
     @Override
