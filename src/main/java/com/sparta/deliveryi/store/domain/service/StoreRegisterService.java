bdc0a242e380a6ef467a02c92aff9fd8eb0fb16b
devangelwings <khs96523@gmail.com>
2025-10-31 16:21:52 +0900

    FEAT: 가게 등록 도메인 서비스 @Validated 추가
    See also: #5

--- src/main/java/com/sparta/deliveryi/store/domain/service/StoreRegisterService.java
+++ src/main/java/com/sparta/deliveryi/store/domain/service/StoreRegisterService.java
@@ -3,12 +3,14 @@ package com.sparta.deliveryi.store.domain.service;
 import com.sparta.deliveryi.store.domain.Store;
 import com.sparta.deliveryi.store.domain.StoreRegisterRequest;
 import com.sparta.deliveryi.store.domain.StoreRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 @Service
 @Transactional
+@Validated
 @RequiredArgsConstructor
 public class StoreRegisterService implements StoreRegister {
 
