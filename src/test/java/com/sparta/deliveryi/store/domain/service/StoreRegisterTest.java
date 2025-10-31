aa238f3f3a9cf195c1370dd7ccf913c3a741026c
devangelwings <khs96523@gmail.com>
2025-10-31 16:23:36 +0900

    TEST: StoreRegister 테스트 코드 생성자 주입 적용
    See also: #5

--- src/test/java/com/sparta/deliveryi/store/domain/service/StoreRegisterTest.java
+++ src/test/java/com/sparta/deliveryi/store/domain/service/StoreRegisterTest.java
@@ -6,20 +6,13 @@ import com.sparta.deliveryi.store.domain.StoreStatus;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-class StoreRegisterTest {
-
-    @Autowired
-    private StoreRegister storeRegister;
-
-    @Autowired
-    private EntityManager entityManager;
+record StoreRegisterTest(StoreRegister storeRegister, EntityManager entityManager) {
 
     @Test
     void register() {
