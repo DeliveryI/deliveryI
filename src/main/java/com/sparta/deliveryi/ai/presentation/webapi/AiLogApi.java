b466c7f87c0d3fb43d535335cb9b5e2543524e24
nineeko <gkxm1013@naver.com>
2025-10-31 14:33:25 +0900

    FEAT: Ai log api 구현 See also: #11

---
+++ src/main/java/com/sparta/deliveryi/ai/presentation/webapi/AiLogApi.java
@@ -0,0 +1,31 @@
+package com.sparta.deliveryi.ai.presentation.webapi;
+
+import com.sparta.deliveryi.ai.application.service.AiLogService;
+import com.sparta.deliveryi.ai.infrastructure.dto.AiLogRequest;
+import com.sparta.deliveryi.ai.infrastructure.dto.AiLogResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/ai/logs")
+@RequiredArgsConstructor
+public class AiLogApi {
+
+    private final AiLogService aiLogService;
+
+    @PostMapping
+    public ResponseEntity<AiLogResponse> createAiLog(
+            @RequestBody AiLogRequest requestDto
+    ) {
+        AiLogResponse response = aiLogService.createAiLog(requestDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
