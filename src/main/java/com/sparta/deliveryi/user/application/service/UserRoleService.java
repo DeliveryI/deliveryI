6f4451598c0a1b2d1f1d3727c11c64bd1806580b
hyeon48165 <hyeon48615@gmail.com>
2025-11-03 16:01:02 +0900

    FIX: 누락된 import 추가

--- src/main/java/com/sparta/deliveryi/user/application/service/UserRoleService.java
+++ src/main/java/com/sparta/deliveryi/user/application/service/UserRoleService.java
@@ -1,5 +1,8 @@
 package com.sparta.deliveryi.user.application.service;
 
+import com.sparta.deliveryi.user.domain.UserId;
+import com.sparta.deliveryi.user.domain.UserRole;
+
 public interface UserRoleService {
     boolean isUserRole(UserId userId, UserRole role);
 
