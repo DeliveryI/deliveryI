24fd819e8a3d9c88a3291de7fba637340a0790b4
hyeon48165 <hyeon48615@gmail.com>
2025-11-03 11:12:56 +0900

    FEAT: User 도메인 모델 구현

---
+++ src/main/java/com/sparta/deliveryi/user/domain/KeycloakId.java
@@ -0,0 +1,30 @@
+package com.sparta.deliveryi.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class KeycloakId {
+
+    @Column(name = "keycloak_id", nullable = false)
+    private UUID id;
+
+    protected KeycloakId(UUID id) { this.id = id;}
+
+    public static KeycloakId of(UUID id) {
+        return new KeycloakId(id);
+    }
+
+    public static KeycloakId of(String id) {
+        return new KeycloakId(UUID.fromString(id));
+    }
+}
