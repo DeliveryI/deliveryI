48705e0454a2ddfb5c62e1594d96f5be939e829b
devangelwings <khs96523@gmail.com>
2025-11-07 19:35:10 +0900

    FIX: 메뉴 저장 테스트

--- src/main/java/com/sparta/deliveryi/menu/application/event/StoreRemoveEventHandler.java
+++ src/main/java/com/sparta/deliveryi/menu/application/event/StoreRemoveEventHandler.java
@@ -1,20 +1,24 @@
 package com.sparta.deliveryi.menu.application.event;
 
 import com.sparta.deliveryi.menu.application.service.MenuAllRemoveService;
+import com.sparta.deliveryi.menu.domain.Menu;
+import com.sparta.deliveryi.menu.domain.repository.MenuRepository;
 import com.sparta.deliveryi.store.event.StoreRemoveEvent;
 import com.sparta.deliveryi.user.domain.UserId;
 import com.sparta.deliveryi.user.domain.service.UserQuery;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StoreRemoveEventHandler {
 
-    private final MenuAllRemoveService menuAllRemoveService;
+    private final MenuRepository menuRepository;
     private final UserQuery userQuery;
 
     @Async
@@ -24,6 +28,16 @@ public class StoreRemoveEventHandler {
 
         String username = user.getUsername();
 
-        menuAllRemoveService.removeAllByStoreId(event.storeId(), username);
+        var menus = menuRepository.findAllByStoreIdAndDeletedAtIsNull(event.storeId());
+
+        log.info("menus size = {}", menus.size());
+
+        menus.forEach(menu -> {
+            log.info("menu name = {}", menu.getMenuName());
+            menu.markDeleted(username);
+            Menu result = menuRepository.save(menu);
+            log.info("finish = {}", result.getDeletedBy());
+        });
+//        menuAllRemoveService.removeAllByStoreId(event.storeId(), username);
     }
 }
