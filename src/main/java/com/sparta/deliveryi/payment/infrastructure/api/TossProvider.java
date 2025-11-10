e382b908c6c28a7ec2c237fa0681623d6852f5d7
hyeon48165 <hyeon48615@gmail.com>
2025-11-10 09:01:21 +0900

    FIX: payment, transaction 오류 해결

--- src/main/java/com/sparta/deliveryi/payment/infrastructure/api/TossProvider.java
+++ src/main/java/com/sparta/deliveryi/payment/infrastructure/api/TossProvider.java
@@ -14,8 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Map;
 
 @Log4j2
@@ -31,8 +29,7 @@ class TossProvider {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        byte[] encodedBytes = Base64.getEncoder().encode((properties.getEncodedSecretKey() + ":").getBytes(StandardCharsets.UTF_8));
-        headers.set("Authorization", "Basic " + new String(encodedBytes, StandardCharsets.UTF_8));
+        headers.set("Authorization", "Basic " + properties.getEncodedSecretKey());
         // headers.set("Idempotency-Key", properties.getIdempotencyKey());
 
         return headers;
